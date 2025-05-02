12486cf78b1bcf54958144fc731da398c85679ce
Vadim Savin <savinvadim1312@gmail.com>
2025-05-02 16:13:56 +0200

    path aliases

--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -4,7 +4,6 @@ export default function HomeScreen() {
   return (
     <View className='flex-1 p-4'>
       <Text className='text-3xl font-bold text-white'>Hello world</Text>
-      <Button />
     </View>
   );
 }
